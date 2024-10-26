a8533d7fd40816f4db4962aa5cba47528b1e2522
Ahmed-Elassa1 <ahmedhamed2150@gmail.com>
2024-10-26 20:15:57 +0300

    #fix: remove unused imported components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import LayoutOne from "./components/layouts/LayoutOne";
 import {
   Home,
@@ -13,21 +13,41 @@ import {
 } from "./pages/index";
 
 function App() {
+  const routers = createHashRouter(
+    [
+      { path: "/signin", element: <Signin /> },
+      { path: "/signup", element: <Signup /> },
+      { path: "/verify-email", element: <VerifyEmail /> },
+      { path: "/reset", element: <Reset /> },
+      { path: "/lock", element: <Lock /> },
+      {
+        path: "/",
+        element: <LayoutOne />,
+        children: [
+          {
+            path: "/",
+            element: <Home />,
+          },
+          {
+            path: "/home",
+            element: <Home />,
+          },
+          {
+            path: "/trade",
+            element: <Trade />,
+          },
+          { path: "/wallet", element: <Wallet /> },
+          { path: "/settings-profile", element: <Profile /> },
+        ],
+      },
+    ],
+    {
+      basename: "/",
+    }
+  );
   return (
     <div className="app">
-      <Routes>
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/verify-email" element={<VerifyEmail />} />
-        <Route path="/reset" element={<Reset />} />
-        <Route path="/lock" element={<Lock />} />
-        <Route path="/" element={<LayoutOne />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/trade" element={<Trade />} />
-          <Route path="/wallet" element={<Wallet />} />
-          <Route path="/settings-profile" element={<Profile />} />
-        </Route>
-      </Routes>
+      <RouterProvider router={routers} />
     </div>
   );
 }
